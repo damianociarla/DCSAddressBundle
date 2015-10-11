bed724df6ab8d09ccac0e43be0a95af9b9214f59
Damiano Ciarla <damiano.ciarla@gmail.com>
2015-10-11 09:58:19 +0200

    Update main file

--- DCSAddressBundle.php
+++ DCSAddressBundle.php
@@ -27,25 +27,27 @@ class DCSAddressBundle extends Bundle
             );
         }
 
-        $mongoCompilerClass = 'Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass';
-        if (class_exists($mongoCompilerClass)) {
-            $container->addCompilerPass(
-                DoctrineMongoDBMappingsPass::createXmlMappingDriver($mappings)
-            );
-        }
+        /* Not yet implemented */
 
-        $couchCompilerClass = 'Doctrine\Bundle\CouchDBBundle\DependencyInjection\Compiler\DoctrineCouchDBMappingsPass';
-        if (class_exists($couchCompilerClass)) {
-            $container->addCompilerPass(
-                DoctrineCouchDBMappingsPass::createXmlMappingDriver($mappings)
-            );
-        }
-
-        $phpcrCompilerClass = 'Doctrine\Bundle\PHPCRBundle\DependencyInjection\Compiler\DoctrinePhpcrMappingsPass';
-        if (class_exists($phpcrCompilerClass)) {
-            $container->addCompilerPass(
-                DoctrinePhpcrMappingsPass::createXmlMappingDriver($mappings)
-            );
-        }
+//        $mongoCompilerClass = 'Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass';
+//        if (class_exists($mongoCompilerClass)) {
+//            $container->addCompilerPass(
+//                DoctrineMongoDBMappingsPass::createXmlMappingDriver($mappings)
+//            );
+//        }
+//
+//        $couchCompilerClass = 'Doctrine\Bundle\CouchDBBundle\DependencyInjection\Compiler\DoctrineCouchDBMappingsPass';
+//        if (class_exists($couchCompilerClass)) {
+//            $container->addCompilerPass(
+//                DoctrineCouchDBMappingsPass::createXmlMappingDriver($mappings)
+//            );
+//        }
+//
+//        $phpcrCompilerClass = 'Doctrine\Bundle\PHPCRBundle\DependencyInjection\Compiler\DoctrinePhpcrMappingsPass';
+//        if (class_exists($phpcrCompilerClass)) {
+//            $container->addCompilerPass(
+//                DoctrinePhpcrMappingsPass::createXmlMappingDriver($mappings)
+//            );
+//        }
     }
 }
